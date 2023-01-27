d6ce9561a40c0b375fecde0c076d11f7f939d1b4
Leafar NecroMaster <leafarnecromaster@gmail.com>
2023-01-27 15:09:08 -0600

    Minor Changes

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -7,9 +7,9 @@ map('n', '<leader><leader>x', ':x<cr>', {noremap = true, silent = true})
 map('n', '<leader><leader>q<leader>', ':q<cr>', {noremap = true, silent = true})
 
 --Fzf.vim
-map('n','<leader><leader>l', ':Lines<cr>', {noremap = true, silent = true})
+map('n','<leader><leader>l', ':BLines<cr>', {noremap = true, silent = true})
 map('n','<leader><leader>fl', ':Files<cr>', {noremap = true, silent = true})
-map('n','<leader><leader>bf', ':BFiles<cr>', {noremap = true, silent = true})
+map('n','<leader><leader>bf', ':Buffers<cr>', {noremap = true, silent = true})
 
 --easymotion
 map('n','<leader><leader>s', '<Plug>(easymotion-s2)', {noremap = true, silent = true})
